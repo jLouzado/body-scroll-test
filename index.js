255de24160fca3d92246146681705c9b41dd60b8
Joel <jlouzado@gmail.com>
2020-03-07 10:03:16 +0530

    feat(pane): generate three lists

---
+++ index.js
@@ -0,0 +1,31 @@
+const cardSizes = ['small', 'medium', 'large']
+const listLengths = [3, 15, 30]
+
+const NUM_AVAILABLE_COLORS = 5
+
+const PANES = ['pane1', 'pane2', 'pane3']
+
+// number => number
+const rand = limit => Math.floor(Math.random() * limit)
+
+// T[] => T
+const sample = arr => arr[rand(arr.length)]
+
+PANES.forEach(pane => {
+  const length = sample(listLengths)
+  const target = document.getElementById(pane)
+
+  const cards = Array(length)
+    .fill(null)
+    .map(el => {
+      const size = sample(cardSizes)
+      const color = rand(NUM_AVAILABLE_COLORS)
+      const card = document.createElement('div')
+      card.classList.add('card', `card--${size}`, `card--color${color + 1}`)
+
+      return card
+    })
+    .forEach(card => {
+      target.appendChild(card)
+    })
+})
